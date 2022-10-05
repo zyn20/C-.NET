26e678964e030e8c0f400f9e27ccb9b25ade5d36
Zain Ul Abideen <79300060+zyn20@users.noreply.github.com>
2022-10-05 13:43:21 +0500

    Create objects.cs
    Types to Create Objects in C#

---
+++ objects.cs
@@ -0,0 +1,15 @@
+using System;
+namespace application;
+
+class Human
+{
+    private int age;
+    private int rating;
+    static void Main(string[] args)
+    {
+
+        //There are 2 ways to create the object in c# 
+        Human zain = new Human();//method 1
+        var david = new Human();//method 2
+    }
+}
